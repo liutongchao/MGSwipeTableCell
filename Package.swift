c1af3e6b58b3cc79af12e4869a69b3174ed96a73
liutongchao <413281269@qq.com>
2020-01-29 12:23:55 +0800

    支持SPM

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "MGSwipeTableCell",
+    products: [
+        .library(
+            name: "MGSwipeTableCell",
+            targets: ["MGSwipeTableCell"]),
+    ],
+    targets: [
+        .target(name: "MGSwipeTableCell",path: "MGSwipeTableCell")
+    ],
+    swiftLanguageVersions: [.v5]
+)
